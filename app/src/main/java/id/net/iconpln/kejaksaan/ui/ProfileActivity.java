9bb9d0c26012f1b776f4458e5d4b80dc3ae8f7bd
rhedella <rhedella25@gmail.com>
2017-04-11 17:53:18 +0700

    Add layout About Add layout Profile Add Activity About Add Activity Profile

---
+++ app/src/main/java/id/net/iconpln/kejaksaan/ui/ProfileActivity.java
@@ -0,0 +1,24 @@
+package id.net.iconpln.kejaksaan.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import id.net.iconpln.kejaksaan.R;
+import id.net.iconpln.kejaksaan.utility.CommonUtils;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CommonUtils.installToolbar(this);
+    }
+
+}
