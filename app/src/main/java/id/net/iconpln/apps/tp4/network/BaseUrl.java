457b511a9d3d02ebecbfe6f5dad2d6f64dcdab1b
adhiozcan <wahyuad10541@gmail.com>
2017-06-14 14:28:44 +0700

    Change base url to public address

--- app/src/main/java/id/net/iconpln/apps/tp4/network/BaseUrl.java
+++ app/src/main/java/id/net/iconpln/apps/tp4/network/BaseUrl.java
@@ -6,7 +6,8 @@ package id.net.iconpln.apps.tp4.network;
 
 public class BaseUrl {
     //Define protocol/scheme for this apps
-    protected static final String BASE_URL  = "http://10.14.153.76:8080";
+    //protected static final String BASE_URL  = "http://10.14.153.76:8080";
+    protected static final String BASE_URL  = "http://apps.iconpln.co.id:9094";
     protected static final String END_POINT = "mobile/";
 
 }
